8fdb8ad6099a68c5c3fa45d6fbccc287c61b9240
Rosamund <rosamund.pearce@carbonbrief.org>
2017-09-01 16:22:04 +0100

    remove tooltip div

--- script.js
+++ script.js
@@ -4442,7 +4442,7 @@ function initialDraw (state) {
                     div.transition()
                         .duration(200)	
                         .style("opacity", 1);	
-                    div	.html("<b>Capacity (MW): </b>" + commaFormat(d[state]))	 
+                    div	.html("<b>Capacity: </b>" + commaFormat(d[state]) + " MW")	 
                         .style("left", (d3.event.pageX) + "px")			 
                         .style("top", (d3.event.pageY - 28) + "px");
                     })
@@ -4544,6 +4544,8 @@ function draw (state) {
                 .attr("height", y.rangeBand())
                 .attr("width", function (d) { return x(d[state])})
                 .style("fill",function(d) {return colorScale(d.Type);});
+
+            // ensure that tooltip changes with data
                 
             var tooltipUpdate = bar.select("rect")
                 .on("mouseover", function(d) {		
@@ -4553,7 +4555,7 @@ function draw (state) {
                     div.transition()
                         .duration(200)	
                         .style("opacity", 1);	
-                    div	.html("<b>Capacity (MW): </b>" + commaFormat(d[state]))	 
+                    div	.html("<span id='#capacity'><b>Capacity: </b></span>" + commaFormat(d[state]) + " MW")	 
                         .style("left", (d3.event.pageX) + "px")			 
                         .style("top", (d3.event.pageY - 28) + "px");
                     })
