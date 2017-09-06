04914a91170cfb6da3c468a89807a23ae08e4971
Rosamund <rosamund.pearce@carbonbrief.org>
2017-09-06 11:50:52 +0100

    add base maps dropdown

--- script.js
+++ script.js
@@ -9,6 +9,36 @@ var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 });
 
+var NASAGIBS_ViirsEarthAtNight2012 = L.tileLayer('http://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}', {
+	attribution: 'Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href="https://earthdata.nasa.gov">ESDIS</a>) with funding provided by NASA/HQ.',
+	bounds: [[-85.0511287776, -179.999999975], [85.0511287776, 179.999999975]],
+	minZoom: 1,
+	maxZoom: 8,
+	format: 'jpg',
+	time: '',
+	tilematrixset: 'GoogleMapsCompatible_Level'
+});
+
+$("#basemaps").change(function () {
+    if (this.value == "Esri_WorldGrayCanvas") {
+        map.removeLayer(Esri_WorldImagery)
+        .removeLayer(NASAGIBS_ViirsEarthAtNight2012)
+        .addLayer(Esri_WorldGrayCanvas);
+    }
+    else if (this.value == "Esri_WorldImagery") {
+        map.removeLayer(Esri_WorldGrayCanvas)
+        .removeLayer(NASAGIBS_ViirsEarthAtNight2012)
+        .addLayer(Esri_WorldImagery);
+
+        console.log("satellite day change");
+    }
+    else if (this.value == "NASAGIBS_ViirsEarthAtNight2012") {
+        map.removeLayer(Esri_WorldGrayCanvas)
+        .removeLayer(Esri_WorldImagery)
+        .addLayer(NASAGIBS_ViirsEarthAtNight2012);
+    }
+});
+
 //disable scroll wheel zoom
 
 map.scrollWheelZoom.disable();
@@ -4571,7 +4601,7 @@ function draw (state) {
 
 }
 
-// redraw d3 graph on window resize
+// redraw d3 graph on window resize - currently not working particularly well but at least with this version it doesn't overlap as the width of the svg itself updates
 
 $(window).on("resize", function () {
 
