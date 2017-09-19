6a60f7765a58a0df95ebf63cc16014ae5d0c5f0e
Rosamund <rosamund.pearce@carbonbrief.org>
2017-09-19 14:27:12 +0100

    add larger logo for desktop

--- script.js
+++ script.js
@@ -4899,7 +4899,7 @@ function drawChart2 (state) {
         .attr('x', width + margin.left - 5)
         .attr('y', 20)
         .style("text-anchor", "end")
-        .style("fill", "#ADADAD")
+        .style("fill", "#898989")
         .text("High carbon");
     
 });
